64cb720b7f2195ef4f10be6d95646df6053ec476
Theodore Terdes <tterdes@exist.com>
2013-05-30 00:20:57 +0800

    Invoice default transaction type to s

--- lib/saasu/invoice.rb
+++ lib/saasu/invoice.rb
@@ -1,38 +1,39 @@
 module Saasu
-  
+
   class Invoice < Transaction
 
-    elements "transactionType" => :string,
-            "invoiceType" => :string,
-            "date" => :string,
-            "contactUid" => :string,
-            "shipToContactUid" => :integer,
-            "folderUid" => :integer,
-            "tags" => :string,
-            "reference" => :string,
-            "summary" => :string,
-            "ccy" => :string,
-            "autoPopulateFxRate" => :boolean,
-            "fcToBxFxRate" => :decimal,
-            "notes" => :string,
-            "externalNotes" => :string,
-            "requiresFollowUp" => :boolean,
-            "dueOrExpiryDate" => :date,
-            "tradingTerms" => :TradingTerms,
-            "layout" => :string,
-            "status" => :string,
-            "invoiceNumber" => :string,
-            "purchaseOrderNumber" => :string,
-            "invoiceItems" => :array,
-            "quickPayment" => :array,
-            "payments" => :array,
-            "totalAmountInclTax" => :decimal,
-            "totalAmountExclTax" => :decimal,
-            "totalTaxAmount" => :decimal,
-            "isSent" => :boolean
+    elements  "transactionType"     => :string,
+              "invoiceType"         => :string,
+              "date"                => :string,
+              "contactUid"          => :string,
+              "shipToContactUid"    => :integer,
+              "folderUid"           => :integer,
+              "tags"                => :string,
+              "reference"           => :string,
+              "summary"             => :string,
+              "ccy"                 => :string,
+              "autoPopulateFxRate"  => :boolean,
+              "fcToBxFxRate"        => :decimal,
+              "notes"               => :string,
+              "externalNotes"       => :string,
+              "requiresFollowUp"    => :boolean,
+              "dueOrExpiryDate"     => :date,
+              "tradingTerms"        => :TradingTerms,
+              "layout"              => :string,
+              "status"              => :string,
+              "invoiceNumber"       => :string,
+              "purchaseOrderNumber" => :string,
+              "invoiceItems"        => :array,
+              "quickPayment"        => :array,
+              "payments"            => :array,
+              "totalAmountInclTax"  => :decimal,
+              "totalAmountExclTax"  => :decimal,
+              "totalTaxAmount"      => :decimal,
+              "isSent"              => :boolean
 
     required_fields %w(uid lastUpdatedUid), :only => :update
     required_fields %w(transactionType invoiceType date layout status)
+    defaults :query_options => { :transaction_type => "s" }
   end
 
 end
