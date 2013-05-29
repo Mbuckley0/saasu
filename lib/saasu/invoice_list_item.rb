94acc612a97d6a9acbefca383a16f4fa2d761145
Theodore Terdes <tterdes@exist.com>
2013-05-30 00:22:36 +0800

    Required fields for Item Invoice Item

--- lib/saasu/invoice_list_item.rb
+++ lib/saasu/invoice_list_item.rb
@@ -1,41 +1,41 @@
 module Saasu
-  
+
   class InvoiceListItem < Entity
-    
+
     elements "lastUpdatedUid"            => :string,
-           "ccy"                       => :string,
-           "autoPopulateFXRate"        => :boolean,
-           "fcToBcFxRate"              => :decimal,
-           "transactionType"           => :string,
-           "invoiceDate"               => :date,
-           "utcFirstCreated"           => :date,
-           "utcLastModified"           => :date,
-           "summary"                   => :string,
-           "invoiceNumber"             => :string,
-           "purchaseOrderNumber"       => :string,
-           "dueDate"                   => :date,
-           "totalAmountInclTax"        => :decimal,
-           "paymentCount"              => :integer,
-           "totalAmountPaid"           => :integer,
-           "amountOwed"                => :decimal,
-           "paidStatus"                => :string,
-           "requiresFollowUp"          => :boolean,
-           "isSent"                    => :boolean,
-           "invoiceLayout"             => :string,
-           "invoiceStatus"             => :string,
-           "contactUid"                => :integer,
-           "contactGivenName"          => :string,
-           "contactFamilyName"         => :string,
-           "contactOrganisationName"   => :string,
-           "shipToContactUid"          => :string,
-           "shipToContactFirstname"    => :string,
-           "shipToContactLastName"     => :string,
-           "shipToContactOrganisation" => :string,
-           "tags"                      => :string
-     
-      # [CHRISK] not used yet 
+             "ccy"                       => :string,
+             "autoPopulateFXRate"        => :boolean,
+             "fcToBcFxRate"              => :decimal,
+             "transactionType"           => :string,
+             "invoiceDate"               => :date,
+             "utcFirstCreated"           => :date,
+             "utcLastModified"           => :date,
+             "summary"                   => :string,
+             "invoiceNumber"             => :string,
+             "purchaseOrderNumber"       => :string,
+             "dueDate"                   => :date,
+             "totalAmountInclTax"        => :decimal,
+             "paymentCount"              => :integer,
+             "totalAmountPaid"           => :integer,
+             "amountOwed"                => :decimal,
+             "paidStatus"                => :string,
+             "requiresFollowUp"          => :boolean,
+             "isSent"                    => :boolean,
+             "invoiceLayout"             => :string,
+             "invoiceStatus"             => :string,
+             "contactUid"                => :integer,
+             "contactGivenName"          => :string,
+             "contactFamilyName"         => :string,
+             "contactOrganisationName"   => :string,
+             "shipToContactUid"          => :string,
+             "shipToContactFirstname"    => :string,
+             "shipToContactLastName"     => :string,
+             "shipToContactOrganisation" => :string,
+             "tags"                      => :string
+
+      # [CHRISK] not used yet
       defaults :query_options => { :transaction_type => "s" }
   end
-  
+
 end
 
